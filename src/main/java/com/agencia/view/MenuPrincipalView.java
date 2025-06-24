850de043f84bf6cfe1fb9e0055f8ea8baeefcc9e
Davi Levy <daviklevy@gmail.com>
2025-06-24 12:29:41 -0300

    Criando interface do menu inicial

---
+++ src/main/java/com/agencia/view/MenuPrincipalView.java
@@ -0,0 +1,125 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MenuPrincipalView extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipalView frame = new MenuPrincipalView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuPrincipalView() {
+		setTitle("Agência de Viagens - Menu Principal");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 428, 317);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel lblNewLabel = new JLabel("Bem-vindo à Agência de Viagens!");
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 18));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		panel.add(verticalStrut_1);
+		
+		JButton btnGerenciarCli = new JButton("Gerenciar Clientes");
+		btnGerenciarCli.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Botão Gerenciar Clientes clicado!");
+			}
+		});
+		btnGerenciarCli.setAlignmentY(Component.TOP_ALIGNMENT);
+		btnGerenciarCli.setFont(new Font("Verdana", Font.BOLD, 14));
+		panel.add(btnGerenciarCli);
+		
+		Component verticalStrut = Box.createVerticalStrut(20);
+		panel.add(verticalStrut);
+		
+		JButton btnGerenciarPac = new JButton("Gerenciar Pacotes");
+		btnGerenciarPac.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Botão Gerenciar Pacotes clicado!");
+			}
+		});
+		btnGerenciarPac.setFont(new Font("Verdana", Font.BOLD, 14));
+		panel.add(btnGerenciarPac);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(20);
+		panel.add(verticalStrut_2);
+		
+		JButton btnGerenciarSer = new JButton("Gerenciar Serviços");
+		btnGerenciarSer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Botão Gerenciar Serviços clicado!");
+			}
+		});
+		btnGerenciarSer.setFont(new Font("Verdana", Font.BOLD, 14));
+		panel.add(btnGerenciarSer);
+		
+		Component verticalStrut_3 = Box.createVerticalStrut(20);
+		panel.add(verticalStrut_3);
+		
+		JButton btnContratar = new JButton("Contratar");
+		btnContratar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Botão Contratar clicado!");
+			}
+		});
+		btnContratar.setFont(new Font("Verdana", Font.BOLD, 14));
+		panel.add(btnContratar);
+		
+		Component verticalStrut_4 = Box.createVerticalStrut(20);
+		panel.add(verticalStrut_4);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnSair.setFont(new Font("Verdana", Font.BOLD, 14));
+		panel.add(btnSair);
+
+	}
+
+}
